389035a23f342e4009325f672d946bfcbbb75522
Manuel <manuel.korrmann@gmail.com>
2020-12-30 22:14:11 +0100

    Syso durch logger ersetzt

--- src/main/java/gecko/listeners/ReadyListener.java
+++ src/main/java/gecko/listeners/ReadyListener.java
@@ -1,21 +1,25 @@
 package gecko.listeners;
 
+import gecko.actions.ChangeVolumeAction;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReadyListener implements EventListener {
+    private static final Logger LOG = LoggerFactory.getLogger(ChangeVolumeAction.class);
 
     @Override
     public void onEvent(@NotNull GenericEvent event) {
         if (event instanceof ReadyEvent) {
-            System.out.println("API is ready!");
+            LOG.info("API is ready!");
             for (Guild guild : event.getJDA().getGuilds()) {
                 for (GuildChannel channel : guild.getChannels()) {
-                    System.out.printf("%s(%d)%n", channel.getName(), channel.getIdLong());
+                    LOG.info("%s(%d)".formatted(channel.getName(), channel.getIdLong()));
                 }
             }
         }
